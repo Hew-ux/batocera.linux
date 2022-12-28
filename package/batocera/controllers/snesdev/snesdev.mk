2c9f3b6faaff799e04ef6333f7837951ff5f76ad
Atari <67527064+Hew-ux@users.noreply.github.com>
2022-12-28 18:27:30 +1100

    switch to axle-h fork

--- package/batocera/controllers/snesdev/snesdev.mk
+++ package/batocera/controllers/snesdev/snesdev.mk
@@ -5,12 +5,13 @@
 ################################################################################
 # Version 1.0
 # Released Sep 5, 2019
-SNESDEV_VERSION = c09df177b6a4e1716d2c84f5a8c7ed0c24a730d8
-SNESDEV_SITE = $(call github,petrockblog,SNESDev-RPi,$(SNESDEV_VERSION))
+SNESDEV_VERSION = 4ab4be71e7a8586746f23e52a9d8d5dfa73ef2a4
+SNESDEV_SITE = $(call github,axle-h,SNESDev-RPi,$(SNESDEV_VERSION))
+SNESDEV_DEPENDENCIES = libconfuse-dev
 
 # No dependencies because apparently this project's makefile makes them on its own (?)
 
 #define SNESDEV_INSTALL_TARGET_CMDS
 #endef
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
